a76a0149e6f1eed46b2b9a0765f6e7244ffeb5b8
yixiang3 <yixiang3@student.unimelb.edu.au>
2023-10-03 22:16:35 +1100

    Add some more UserSongListTests

--- src/test/java/com/team69/itproject/UserSongListTests.java
+++ src/test/java/com/team69/itproject/UserSongListTests.java
@@ -10,6 +10,7 @@ import com.team69.itproject.entities.dto.SongDTO;
 import com.team69.itproject.entities.dto.UsersDTO;
 import com.team69.itproject.entities.po.SongPO;
 import com.team69.itproject.entities.po.UserPO;
+import com.team69.itproject.entities.po.UserSongListPO;
 import com.team69.itproject.entities.vo.SongVO;
 import com.team69.itproject.mappers.SongMapper;
 import com.team69.itproject.services.SongService;
@@ -72,6 +73,7 @@ class UserSongListTests {
         usersService.save(newUser);
         String songName = "ABC";
         int n = 20;
+        int m=4;
         for (int i = 0; i < n; i++) {
             SongVO songVO = new SongVO(songName + i, "aaa", "DEF", LocalDate.now());
             SongPO newSong = new SongPO();
@@ -79,7 +81,7 @@ class UserSongListTests {
             songService.save(newSong);
         }
         List<Long> ids = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < m; i++) {
             Page<SongDTO> songPOPage = new Page<>(0, 20);
             Page<SongDTO> songs = songService.searchSongByName(songPOPage, songName+i);
             long id = songs.getRecords().get(0).getId();
@@ -93,9 +95,119 @@ class UserSongListTests {
 
         //
         songDAO.addSongToUserSongList(xn.getId(),"Favorate",ids);
-        Page<SongDTO> songPOPage = new Page<>(0, 1);
+        Page<SongDTO> songPOPage = new Page<>(0, 20);
         songService.getUserSongListByName(songPOPage, xn.getId(), "Favorate");
-        System.out.println(songPOPage.getRecords().get(0).getName());
+        for (int i = 0; i < m; i++) {
+            assert (songPOPage.getRecords().get(i).getName().equals(songName+i));
+        }
+
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void testGetUserSongListByName() {
+        String name = "XNXNXN";
+        deleteAllUser();
+        deleteAllSong();
+        UserPO newUser = UserPO.builder()
+                .username(name)
+                .email("aaa@gmail.com")
+                .password(passwordEncoder.encode("123456"))
+                .authorities(List.of(new SimpleGrantedAuthority("normal")))
+                .build();
+        usersService.save(newUser);
+        String songName = "ABC";
+        int n = 20;
+        int m=4;
+        for (int i = 0; i < n; i++) {
+            SongVO songVO = new SongVO(songName + i, "aaa", "DEF", LocalDate.now());
+            SongPO newSong = new SongPO();
+            BeanUtil.copyProperties(songVO, newSong);
+            songService.save(newSong);
+        }
+
+        UserPO xn = usersService.getOne(
+                new LambdaQueryWrapper<UserPO>()
+                        .eq(UserPO::getUsername, name)
+        );
+
+        for (int i = 0; i < m; i++) {
+            Page<SongDTO> songPOPage = new Page<>(0, 20);
+            Page<SongDTO> songs = songService.searchSongByName(songPOPage, songName+i);
+            long id = songs.getRecords().get(0).getId();
+            UserSongListPO userSongListPO = new UserSongListPO();
+            userSongListPO.setUserId(xn.getId());
+            userSongListPO.setSongId(id);
+            userSongListPO.setName("Favorate");
+            USLService.save(userSongListPO);
+        }
+
+
+        //
+        Page<SongDTO> songPOPage = songDAO.getUserSongListByName(xn.getId(),"Favorate",0,20);
+        for (int i = 0; i < m; i++) {
+            assert (songPOPage.getRecords().get(i).getName().equals(songName+i));
+        }
+
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void testRemoveSongFromUserSongList() {
+        String name = "XNXNXN";
+        deleteAllUser();
+        deleteAllSong();
+        UserPO newUser = UserPO.builder()
+                .username(name)
+                .email("aaa@gmail.com")
+                .password(passwordEncoder.encode("123456"))
+                .authorities(List.of(new SimpleGrantedAuthority("normal")))
+                .build();
+        usersService.save(newUser);
+        String songName = "ABC";
+        int n = 20;
+        int m=10;
+        int k = 5;
+        for (int i = 0; i < n; i++) {
+            SongVO songVO = new SongVO(songName + i, "aaa", "DEF", LocalDate.now());
+            SongPO newSong = new SongPO();
+            BeanUtil.copyProperties(songVO, newSong);
+            songService.save(newSong);
+        }
+        UserPO xn = usersService.getOne(
+                new LambdaQueryWrapper<UserPO>()
+                        .eq(UserPO::getUsername, name)
+        );
+
+        for (int i = 0; i < m; i++) {
+            Page<SongDTO> songPOPage = new Page<>(0, 20);
+            Page<SongDTO> songs = songService.searchSongByName(songPOPage, songName+i);
+            long id = songs.getRecords().get(0).getId();
+            UserSongListPO userSongListPO = new UserSongListPO();
+            userSongListPO.setUserId(xn.getId());
+            userSongListPO.setSongId(id);
+            userSongListPO.setName("Favorate");
+            USLService.save(userSongListPO);
+        }
+
+        List<Long> ids = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            Page<SongDTO> songPOPage = new Page<>(0, 20);
+            Page<SongDTO> songs = songService.searchSongByName(songPOPage, songName+i);
+            long id = songs.getRecords().get(0).getId();
+            ids.add(id);
+        }
+        songDAO.removeSongFromUserSongList(xn.getId(),"Favorate",ids);
+
+        Page<SongDTO> songPOPage = new Page<>(0, 20);
+        songService.getUserSongListByName(songPOPage, xn.getId(), "Favorate");
+        for (int i = 0; i < m-k; i++) {
+            assert (songPOPage.getRecords().get(i).getName().equals(songName+(k+i)));;
+            assert (!songPOPage.getRecords().get(i).getName().equals(songName+(0)));
+        }
+
     }
 
 }
